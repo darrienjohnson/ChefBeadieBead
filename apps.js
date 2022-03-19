cd364bcf35644f290c1ef328ba64a764b539759e
Darrienrjohnson <darrien.rjohnson@gmail.com>
2022-03-18 20:33:24 -0400

    added menu Modal

--- apps.js
+++ apps.js
@@ -1,7 +1,18 @@
 const hamburger = document.getElementById('hamburger')
 const navLinks = document.getElementById('main-nav')
 const link = document.querySelectorAll('nav-link')
+const menuModalButton = document.querySelector('.modal-btn')
+const modalBg = document.querySelector('.modal-bg')
+const modalClose = document.querySelector('.modal-close')
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
+
+menuModalButton.addEventListener('click', () => {
+  modalBg.classList.add('modal-active')
+});
+modalClose.addEventListener('click', () => {
+  modalBg.classList.remove('modal-active')
+});
+
